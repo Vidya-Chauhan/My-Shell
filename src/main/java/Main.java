209f0b4770520e4e0a0416d1fb531995839a3fe5
Vidya-Chauhan <chauhanvidya327@gmail.com>
2025-06-20 12:02:01 +0530

    stage 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,10 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        System.out.println(input + ": command not found");
+        while(true) {
+            
+            System.out.println(input + ": command not found");
+        }
     }
 }
+
