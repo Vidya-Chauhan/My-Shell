ede7480d1c7ded9045385a66702da788079e444f
Vidya-Chauhan <chauhanvidya327@gmail.com>
2025-06-23 13:59:27 +0530

    stage 11 again

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,21 +2,23 @@ import java.io.File;
 import java.util.Scanner;
 
 public class Main {
-    public static File currentDirectory = new File (System.getProperty("user.dir"));
-    public static void main(String[] args) throws Exception {
-        //Uncomment this block to pass the first stage
-        Scanner scanner = new Scanner(System.in);
-        while(true) {
-            System.out.print("$ ");
-            String input = scanner.nextLine();
-                   
-            
-           if(!Builtins.handleBuiltin(input)){
-               Executor.runExternal(input);
+    public static File currentDirectory = new File(System.getProperty("user.dir"));
 
-            
-           }
-           System.out.flush();
+    public static void printPrompt() {
+        System.out.print("$ ");
+        System.out.flush();  // ✅ Very important
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            printPrompt();
+            String input = scanner.nextLine();
+
+            if (!Builtins.handleBuiltin(input)) {
+                Executor.runExternal(input);
+            }
         }
     }
 }
