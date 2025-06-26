3033a838ec78be736797ba0d5c842f7f5b21f9b3
Vidya-Chauhan <chauhanvidya327@gmail.com>
2025-06-26 14:57:29 +0530

    stage 22

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Terminal terminal = TerminalBuilder.builder()
             .system(true)
+            .dumb(true)
             .build();
 
         // Autocomplete for built-in commands
