4e943baefa216694dba8830f6f9535c62071f970
Vidya-Chauhan <chauhanvidya327@gmail.com>
2025-06-28 00:12:57 +0530

    fix: added missing your_program.sh script

--- your_program.sh
+++ your_program.sh
@@ -1,36 +1,9 @@
-# #!/bin/sh
-# #
-# # Use this script to run your program LOCALLY.
-# #
-# # Note: Changing this script WILL NOT affect how CodeCrafters runs your program.
-# #
-# # Learn more: https://codecrafters.io/program-interface
-
-# set -e # Exit early if any commands fail
-
-# # Copied from .codecrafters/compile.sh
-# #
-# # - Edit this to change how your program compiles locally
-# # - Edit .codecrafters/compile.sh to change how your program compiles remotely
-# (
-#   cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-#   mvn -q -B package -Ddir=/tmp/codecrafters-build-shell-java
-# )
-
-# # Copied from .codecrafters/run.sh
-# #
-# # - Edit this to change how your program runs locally
-# # - Edit .codecrafters/run.sh to change how your program runs remotely
-# exec java -jar /tmp/codecrafters-build-shell-java/codecrafters-shell.jar "$@"
-
-
 #!/bin/sh
 set -e
 
-# Compile Java files from src/main/java to out
+# Compile source code
 mkdir -p out
 javac -d out src/main/java/*.java
 
-# Run the compiled program (no jar involved!)
+# Run program
 java -cp out Main
-
